2876f6913ed14785dcc31f8fbcbd62f1b4842ff1
cichowsky <mrcichowsky@gmail.com>
2021-12-11 13:18:39 +0100

    backend: add cors and nodemon

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const { port } = require("./app/config");
 const router = require("./app/routes/api");
+const cors = require("cors");
 
 // database
 require("./app/db/mongoose");
@@ -9,6 +10,9 @@ require("./app/db/mongoose");
 // parser
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
+
+//cors
+app.use(cors());
 
 // routes
 app.use("/api", router);
