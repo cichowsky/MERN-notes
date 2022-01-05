b24730609cecb36a894590f11dc630d96fddbf76
cichowsky <mrcichowsky@gmail.com>
2022-01-05 16:20:21 +0100

    frontend: modal fixes

--- frontend/src/components/organisms/Modal/Modal.js
+++ frontend/src/components/organisms/Modal/Modal.js
@@ -2,22 +2,21 @@ import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import { ReactComponent as CloseIcon } from 'assets/svg/close.svg';
 
-const overlayClassName = 'fixed inset-0 bg-black bg-opacity-75 z-50';
-const modalClassName =
-  'fixed left-1/2 top-1/2 transform -translate-y-1/2 -translate-x-1/2 w-full max-w-2xl py-5 px-4 bg-white shadow-md rounded-lg z-50';
-const closeBtnClassName = 'fixed p-1 top-0.5 right-0.5 text-red-800 hover:text-red-600';
+const modalOuterClassName = 'fixed inset-0 flex flex-col items-center justify-center z-50';
+const modalBoxClassName =
+  'relative shrink-0 bg-white shadow-md rounded-lg overflow-auto w-full max-w-2xl';
+const closeBtnClassName = 'absolute p-1 top-0.5 right-0.5 text-red-800 hover:text-red-600';
 
 const Modal = ({ children, handleClose = () => {}, isOverlay = true }) => {
   return ReactDOM.createPortal(
-    <>
-      {isOverlay && <span className={overlayClassName} />}
-      <div className={modalClassName}>
+    <div className={`${modalOuterClassName} ${isOverlay && 'bg-black bg-opacity-75'}`}>
+      <div className={modalBoxClassName}>
         <button type="button" className={closeBtnClassName} onClick={handleClose}>
           <CloseIcon />
         </button>
-        <div>{children}</div>
+        <div className="py-5 px-4">{children}</div>
       </div>
-    </>,
+    </div>,
     document.getElementById('modal-container')
   );
 };
